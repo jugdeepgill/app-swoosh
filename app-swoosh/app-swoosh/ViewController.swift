4c146c42542f780c6e70c69536dcffda1530dbab
Jugdeep Gill <jugderp@Kanwardeeps-iMac.local>
2017-07-29 00:04:05 +0800

    Lesson 2 Ends

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var bgImg: UIImageView!
+    @IBOutlet weak var swoosh: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 20, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        bgImg.frame = view.frame;
     }
 
     override func didReceiveMemoryWarning() {
